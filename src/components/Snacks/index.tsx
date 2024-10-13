d1b32129b25c32a06e36a2a2fd6624270b16cc26
lucaslira <lucasliraone@gmail.com>
2024-10-13 14:28:55 -0300

    formatando o preço com lib nativa - helpers

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,6 +1,7 @@
 import { FiPlus } from 'react-icons/fi'
 
 import { Container } from './styles'
+import { currencyFormat } from '../../helpers/currencyFormat'
 
 interface SnacksProps {
   snacks: any[]
@@ -16,7 +17,7 @@ export function Snacks({ snacks } : SnacksProps) {
             <img src={snack.image} alt={snack.name}/>
             <p>{snack.description}</p>
             <div>
-              <strong>R$ {snack.price}</strong>
+              <strong>{currencyFormat(snack.price)}</strong>
               <button type="button">
                 <FiPlus />
               </button>
