735761a305a8d7ff4562fcca085efb5b262081a6
Lucas Lira <lucasliradev@gmail.com>
2024-10-14 16:48:55 -0300

    concluindo carregamento de dados via api - json-server

--- src/pages/Main/Drinks/index.tsx
+++ src/pages/Main/Drinks/index.tsx
@@ -1,42 +1,26 @@
 import { Head } from '../../../components/Head'
 import { SnackTitle } from '../../../components/SnackTitle'
 import { Snacks } from '../../../components/Snacks'
+import { useEffect, useState } from 'react'
+import { getDrinks } from '../../../services/api'
 
 export default function Drinks() {
-  const data = [
-    {
-      id: 1,
-      snack: 'drink',
-      name: 'Sucos',
-      description:
-        'Sucos nos sabores de morango, kiwi, abacaxi, laranja e limão.',
-      price: 8.5,
-      image: 'https://images.unsplash.com/photo-1647275486864-1b29efb0d570?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 2,
-      snack: 'drink',
-      name: 'Refrigerantes',
-      description:
-        'Refrigerantes nos sabores de coca-cola, sprite e fanta.',
-      price: 5.5,
-      image: 'https://images.unsplash.com/photo-1629059042675-ce386c84cebf?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },    {
-      id: 3,
-      snack: 'drink',
-      name: 'Vitaminas',
-      description:
-        'Vitaminas nos sabores de açai, goiaba, banana e camapu.',
-      price: 15.5,
-      image: 'https://images.unsplash.com/photo-1662130187270-a4d52c700eb6?q=80&w=1402&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-  ]
+  const [drinks, setDrinks] = useState([])
+
+  useEffect(() => {
+    (
+      async() => {
+        const response = await getDrinks()
+        setDrinks(response.data)
+      }
+      )()
+  }, [])
 
   return (
     <>
       <Head title='Bebidas' description='Nossos melhores bebidas.'/>
       <SnackTitle>Bebidas</SnackTitle>
-      <Snacks snacks={data}/>
+      <Snacks snacks={drinks}/>
     </>
   )
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
+
 })
 
 export const getBurgers = () => api.get("/burgers")
