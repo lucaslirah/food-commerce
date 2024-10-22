5ceabd7ded0a8ef2956e4f13cc6e4c83a9a4c117
Lucas Lira <lucasliradev@gmail.com>
2024-10-22 16:20:48 -0300

    condição para renderização do TableMobile

---
+++ src/pages/MyCart/Table/TableMobile/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles'
+
+export function TableMobile() {
+  return (
+    <Container>
+      <h1>Mobile</h1>
+    </Container>
+  )
+}
--- src/pages/MyCart/Table/index.tsx
+++ src/pages/MyCart/Table/index.tsx
@@ -1,10 +1,28 @@
 import { useCart } from '../../../hooks/useCart'
 import { TableDesktop } from './TableDesktop'
+import { TableMobile } from './TableMobile'
+import { useEffect, useState } from 'react'
 
 export function Table() {
   const { cart } = useCart()
+  const [ windowWidth, setWindowWidth ] = useState(document.documentElement.clientWidth)
+
+  useEffect(() => {
+    function updateWindowDimensions() {
+      const currentWidth = document.documentElement.clientWidth
+      setWindowWidth(currentWidth)
+    }
+    window.addEventListener('resize', updateWindowDimensions)
+
+    return () => {
+      window.removeEventListener('resize', updateWindowDimensions)
+      console.log(windowWidth)
+    }
+  }, [])
 
   if(cart.length === 0) return <h1>Ops, não há pedidos no carrinho, peça já!</h1>
 
-  return <TableDesktop/>
+  return (
+    windowWidth > 768 ? <TableDesktop /> : <TableMobile />
+  )
 }
